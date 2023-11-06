b7f900231b854c6608b2f2ead4d6126147d53ad4
김남령 <sikk@gimnamlyeong-ui-MacBookPro.local>
2023-11-06 18:40:15 +0900

    refactor: SolcParser 리팩토링

--- antibug/compile/antibug_compile/parse_version_and_install_solc.py
+++ antibug/compile/antibug_compile/parse_version_and_install_solc.py
@@ -1,4 +1,3 @@
-import sys
 import re
 import os
 import requests
@@ -7,79 +6,78 @@ import shutil
 import argparse
 import subprocess
 from pathlib import Path
-from typing import Optional
+from typing import Dict, List, Optional, Union
 
 if "VIRTUAL_ENV" in os.environ:
     HOME_DIR = Path(os.environ["VIRTUAL_ENV"])
 else:
     HOME_DIR = Path.home()
+    
 SOLC_PARSER_DIR = HOME_DIR.joinpath(".solc-select")
 SOLC_BINARIES_DIR = SOLC_PARSER_DIR.joinpath("artifacts")
 
 class SolcParser:
     def __init__(self, target:str):
-        self.source = target
-        self.file_name= os.path.basename(target)
-        self.version_list = list(self.get_version_list().keys())
-        self.solidity_file = self.get_solidity_source(self.source)
-        self.sign, self.version = self.parse_solidity_version(self.solidity_file)
-        self.check = self.check_version(self.version_list, self.version)
-        self.file_path, self.solc_version, self.solc_binary_path = self.parser_main(target)
+        self._target = target
+        self._file_name= os.path.basename(target)
+        self._file_contents = None
+        self._release_version_list = self.release_version_list()
+        
+        self._version_list = self.version_list
+        self._intalled_solc_versions = List[str]
+        self._current_solc_version = Union[str, None]
+        
+        self._solc_binary_path = None
+        self._solc_binary_version= None
 
 
     #######################################
     ############# Information #############
     #######################################
-    def get_solidity_source(self, target):
-        with open(target, 'r') as f:
-            source_code = f.read()
-        return source_code
-
-    def get_version_list(self):
+    @property
+    def file_contents(self) -> str:
+        with open(self._target, 'r') as f:
+            self._file_contents = f.read()
+        return self._file_contents
+    
+    def release_version_list(self) -> Dict[str, str]:
         url = f"https://binaries.soliditylang.org/macosx-amd64/list.json"
         list_json = requests.get(url).content
-        releases = json.loads(list_json)["releases"]
-        return releases
+        self._release_version_list = json.loads(list_json)["releases"]
+        return self._release_version_list
+    
+    @property
+    def version_list(self) -> List[str]:
+        self._version_list = list(self._release_version_list.keys())
+        return self._version_list
 
-    def get_intalled_versions(self):
-        return [str(p.name).replace("solc-", "") for p in SOLC_BINARIES_DIR.iterdir() if p.is_dir()]
 
-    def get_current_version(self):
+    @classmethod
+    def intalled_solc_versions(cls) -> List[str]:
+        cls._intalled_solc_versions = [str(p.name).replace("solc-", "") for p in SOLC_BINARIES_DIR.iterdir() if p.is_dir()]
+        return cls._intalled_solc_versions
+
+    @classmethod
+    def current_solc_version(cls) -> Optional[str]:
         if os.path.exists(f"{SOLC_PARSER_DIR}/global-version"):
             with open(f"{SOLC_PARSER_DIR}/global-version", "r", encoding="utf-8") as f:
-                return f.read()
+                cls._current_solc_version = f.read()
+                return cls._current_solc_version
         else:
             return None
 
-    def get_which_solc(self):
-        solc_path = subprocess.check_output(["which", "solc"], encoding='utf-8').strip()
-        return solc_path
+    @property
+    def solc_binary_path(self) -> str:
+        self._solc_binary_path = subprocess.check_output(["which", "solc"], encoding='utf-8').strip()
+        return self._solc_binary_path
 
-
-    ########################################
-    ############ Check version #############
-    ########################################
-    def check_version(self, version_list, version):
-        for v in version:
-            if v not in version_list:
-                return False
-            else:
-                return True
-
-    def check_installed_version(self, version):
-        for _, _, files in os.walk(SOLC_BINARIES_DIR):
-            for file in files:
-                installed_version = file.split('-')[1]
-                if (installed_version == version):
-                    return True
-        return False
-
+    
     #########################################
     ############# Parse version #############
     #########################################
-    def parse_solidity_version(self, source_code):
+    def parse_version_in_file_contents(self):
         pattern = r".*pragma solidity.*"
-        pragma_lines = re.findall(pattern, source_code)
+        pragma_lines = re.findall(pattern, self.file_contents)
         version = []
         sign = []
         for pragma_match in pragma_lines:
@@ -92,24 +90,42 @@ class SolcParser:
         return sign, version
 
 
+    ########################################
+    ############ Check version #############
+    ########################################
+    def check_version(self, version) -> bool:
+        for v in version:
+            if v not in self._version_list:
+                return False
+            else:
+                return True
+
+    # def check_installed_version(version):
+    #     for _, _, files in os.walk(SOLC_BINARIES_DIR):
+    #         for file in files:
+    #             installed_version = file.split('-')[1]
+    #             if (installed_version == version):
+    #                 return True
+    #     return False
+
     ###############################################
     ############# Select solc version #############
     ###############################################
-    def find_matching_index(self, versions, version_list):
-        for i, v in enumerate(version_list):
+    def find_matching_index(self, versions):
+        for i, v in enumerate(self._version_list):
             if versions == v:
                 return i
         return None
 
-    def compare_version(self, sign_list, version_list):
-        min_version = min(version_list)
-        min_index = version_list.index(min_version)
+    def compare_version(self, sign_list):
+        min_version = min(self._version_list)
+        min_index = self._version_list.index(min_version)
         return list([sign_list[min_index]]), list([min_version])
 
-    def get_highest_version(self, version_list, target_version, target_index):
+    def get_highest_version(self, target_version, target_index):
         matching_versions = []
         target_major_minor = '.'.join(target_version.split('.')[:2])
-        for v in version_list:
+        for v in self._version_list:
             if v.startswith(target_major_minor):
                 matching_versions.append(v)
         target_index = matching_versions.index(target_version)
@@ -119,95 +135,87 @@ class SolcParser:
         else:
             return matching_versions[target_index -1]
 
-
     #########################################################
     ############# Install/Uninstall/Switch solc #############
     #########################################################
-    def install_solc(self, version):
-        artifact_file_dir = SOLC_BINARIES_DIR.joinpath(f"solc-{version}")
+    def install_solc(self):
+        artifact_file_dir = SOLC_BINARIES_DIR.joinpath(f"solc-{self._solc_binary_version}")
         if os.path.exists(artifact_file_dir):
-            #print(f"'{version}' is already installed.")
+            print(f"'{self._solc_binary_version}' is already installed.")
             return artifact_file_dir, False
-        artifacts = self.get_version_list()
+        
+        artifacts = self._release_version_list
         url = f"https://binaries.soliditylang.org/macosx-amd64/" + \
-            artifacts.get(version)
+            artifacts.get(self._solc_binary_version)
         Path.mkdir(artifact_file_dir, parents=True, exist_ok=True)
-        print(f"Installing solc '{version}'...")
+        print(f"Installing solc '{self._solc_binary_version}'...")
 
         response = requests.get(url)
-        with open(artifact_file_dir.joinpath(f"solc-{version}"), "wb") as file:
+        with open(artifact_file_dir.joinpath(f"solc-{self._solc_binary_version}"), "wb") as file:
             file.write(response.content)
 
-        file_path = artifact_file_dir.joinpath(f"solc-{version}")
-        os.chmod(file_path, 0o775)
-        print(f"Version '{version}' installed.")
-        return file_path, True
+        self._solc_binary_path = artifact_file_dir.joinpath(f"solc-{self._solc_binary_version}")
+        os.chmod(self._solc_binary_path, 0o775)
+        print(f"Version '{self._solc_binary_version}' installed.")
 
 
-    def uninstall_solc(self, version):
-        artifact_file_dir = SOLC_BINARIES_DIR.joinpath(f"solc-{version}")
+    def uninstall_solc(self):
+        artifact_file_dir = SOLC_BINARIES_DIR.joinpath(f"solc-{self._solc_binary_version}")
         if os.path.exists(artifact_file_dir):
-            print(f"Uninstalling solc '{version}'...")
+            print(f"Uninstalling solc '{self._solc_binary_version}'...")
             shutil.rmtree(artifact_file_dir)
-            print(f"Version '{version}' uninstalled.")
-            if version == self.get_current_version():
+            print(f"Version '{self._solc_binary_version}' uninstalled.")
+            if self._solc_binary_version == self.current_solc_version():
                 os.remove(f"{SOLC_PARSER_DIR}/global-version")
                 print(
-                    f"Version '{version}' was the global version. Switching to version.")
+                    f"Version '{self._solc_binary_version}' was the global version. Switching to version.")
         else:
             print(
-                f"'{version}' is not installed. Use 'solc-parser --list' to see all available versions.")
+                f"'{self._solc_binary_version}' is not installed. Use 'solc-parser --list' to see all available versions.")
             return
 
-
-    def switch_global_version(self, version: str, always_install: bool) -> None:
-        if version in self.get_intalled_versions():
+    def switch_global_version(self, always_install: bool) -> None:
+        if self._solc_binary_version in self.intalled_solc_versions():
             with open(f"{SOLC_PARSER_DIR}/global-version", "w", encoding="utf-8") as f:
-                f.write(version)
+                f.write(self._solc_binary_version)
             #print("Switched global version to", version)
-        elif version in self.get_version_list():
+        elif self._solc_binary_version in self._version_list():
             if always_install:
-                self.install_solc([version])
-                self.switch_global_version(version, always_install)
+                self.install_solc([self._solc_binary_version])
+                self.switch_global_version(self._solc_binary_version, always_install)
             else:
                 raise argparse.ArgumentTypeError(
-                    f"'{version}' must be installed prior to use.")
+                    f"'{self._solc_binary_version}' must be installed prior to use.")
         else:
-            raise argparse.ArgumentTypeError(f"Unknown version '{version}'")
-
-
-    def parser_main(self, file_path):
-        if self.check == False:
+            raise argparse.ArgumentTypeError(f"Unknown version '{self._solc_binary_version}'")
+   
+    def run_parser(self):
+        sign, version = self.parse_version_in_file_contents()
+        
+        if self.check_version(version) == False:
             print("incorrect version")
             return
         
-        if len(self.version) != 1:
-            self.sign, self.version = self.compare_version(self.sign, self.version)
+        if len(version) != 1:
+            sign, version = self.compare_version(sign, version)
 
-        sign = self.sign[0]
-        version = self.version[0]
-        index = self.find_matching_index(version, self.version_list)
+        sign = sign[0]
+        version = version[0]
+        index = self.find_matching_index(version)
 
         if sign == '<':
-            solc_version = self.version_list[index - 1]
-            solc_binary_path, _ = self.install_solc(solc_version)
-            self.switch_global_version(solc_version, True)
+            solc_version = self._version_list[index - 1]
         elif sign == '>':
-            solc_version = self.version_list[index + 1]
-            solc_binary_path, _ = self.install_solc(solc_version)
-            self.switch_global_version(solc_version, True)
+            solc_version = self._version_list[index + 1]
         elif (sign == '^' or sign == '~'):
-            solc_version = self.get_highest_version(self.version_list, version, index)
-            solc_binary_path, _= self.install_solc(solc_version)
-            self.switch_global_version(solc_version, True)
+            solc_version = self.get_highest_version(version, index)
         elif (sign == '=' or sign == '>=' or sign == '<=') or (not sign and version):
             solc_version = version
-            solc_binary_path, _ = self.install_solc(solc_version)
-            self.switch_global_version(solc_version, True)
         else:
             print("incorrect sign")
             return
-        return file_path, solc_version, solc_binary_path
+        
+        self._solc_binary_version = solc_version
+        self.install_solc()
+        self.switch_global_version(True)
 
-# instance = SolcParser(sys.argv[1])
-# print(instance.file_path)
